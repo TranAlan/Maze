08c3eea0efb227e876a74426fcd3b1b6d039dda3
AlanTranUO <alantran29@gmail.com>
2016-09-24 22:22:07 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,29 @@
+//ALAN TRAN - Summative Project 2016
+//Date Last Modified : June 17th
+//Project Title: Left 2 Die
+//Project Description: A game that has two settings, one for user input , and one for auto solving.
+
+#include <cstdlib>
+#include <iostream>
+#include "maze.h"
+#include "game.h"
+#include "stack.h"
+
+using namespace std;
+
+
+int main(int argc, char *argv[])
+{
+    Game thegame;
+    thegame.intro();
+    system("PAUSE");
+    return EXIT_SUCCESS;
+    
+}
+
+ //cout<<*(bob.get_pointer_row()) -> item1
+//*(bob.get_pointer_row())
+//*bob.get_pointer_row()
+
+//LAST THING TO DO - CHANGE CharPostitionFunctions to one function, and check functions in hero.
+//COMMENT CONSTRUCTORS
